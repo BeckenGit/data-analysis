890d7073412fdcb0ca2e13ca0b7bc42225074556
Becken <yan_baiqian@founder.com.cn>
2016-07-01 16:22:56 +0800

    modified test.py

--- source/test.py
+++ source/test.py
@@ -1,26 +1,12 @@
 path = '/home/becken/pydata-book/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 import json
-records = [json.loads(line) for line in open(path)]
-time_zones = [rec['tz'] for rec in records if 'tz' in rec]
-
-def get_counts(sequence):
-    counts = {}
-    for x in sequence:
-        if x in counts:
-            counts[x] += 1
-        else:
-            counts[x] = 1
-    return counts
-
-counts = get_counts(time_zones)
-counts['America/New_York']
-
-def top_counts(count_dict, n=10):
-    value_key_pairs = [(count, tz) for tz, count in count_dict.items()]
-    value_key_pairs.sort()
-    return value_key_pairs[-n:]
-
-top_counts(counts)
-
 from pandas import DataFrame, Series
+records = [json.loads(line) for line in open(path)]
 import pandas as pd
+frame = DataFrame(records)
+tz_counts = frame['tz'].value_counts()
+clean_tz = frame['tz'].fillna('Missing')
+clean_tz[clean_tz == ''] = 'Unknown'
+tz_counts = clean_tz.value_counts()
+from pylab import *
+tz_counts[:10].plot(kind='barh', rot=0)
