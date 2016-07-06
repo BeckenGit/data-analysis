67267c4d24f6df0ab4231dae3508b4479db30b91
Becken <517608838@qq.com>
2016-07-06 14:31:53 +0800

    update

--- examples/lsj/plot.py
+++ examples/lsj/plot.py
@@ -1,13 +1,30 @@
 #Transfer the text file to 2D List
 f = open("10.odt")
 allDataStr = f.readlines()
+f.close()
 numLinesStr = allDataStr[12: -1]
-nums = []
+datas = []
 for aStr in numLinesStr:
     aNumsStr = aStr.split()
-    num = []
+    nums = []
     for aNumStr in aNumsStr:
-        num.append(float(aNumStr))
-    nums.append(num)
+        nums.append(float(aNumStr))
+    datas.append(nums)
 
-f.close()
+xSize, ySize, zSize = 400, 50, 1
+
+import numpy as np
+x, y, z = np.meshgrid(np.arange(0, xSize, 1), np.arange(0, ySize, 1), np.arange(0, zSize, 1))
+u = v = w = np.zeros((ySize, xSize, zSize))
+
+for i, data in enumerate(datas):
+    u[i/xSize][i%xSize][0] = data[3]
+    v[i/xSize][i%xSize][0] = data[4]
+    w[i/xSize][i%xSize][0] = data[5]
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+#ax.quiver3D(x, y, z, u, v, w)
+plt.show()
